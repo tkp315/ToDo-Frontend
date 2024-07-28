f244c5ecdf0750015837556557c1f124467a6c35
tkp315 <114278931+tkp315@users.noreply.github.com>
2024-07-28 17:37:56 +0530

    added all the things

--- src/Helpers/axios.js
+++ src/Helpers/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:8003/api/v1"
-const BASE_URL ="http://localhost:5000/api/v1"
+const BASE_URL = "https://todo-list-1-dt3y.onrender.com/api/v1"
+// const BASE_URL ="http://localhost:5000/api/v1"
 
 const axiosInstance =axios.create(
     {
